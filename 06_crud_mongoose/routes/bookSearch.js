8ad6a0abc8a1618af7823eb27defc8197f2049f6
Pratima Gautam <pratimagautam.np@gmail.com>
2024-07-30 17:42:45 +0545

    Deleted to update the folder naming

--- 06_crud_mongoose/routes/bookSearch.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const searchBook = require("../controller/bookSearch");
-
-router.get("/", searchBook);
-
-module.exports = router;
